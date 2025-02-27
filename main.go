4e6a413e3afd49e3b35ae596f5f0b22262a45ea8
Race Malin <racemalin@protonmail.com>
2025-02-26 18:10:58 -0800

    add command functionality with help and exit commands

--- main.go
+++ main.go
@@ -1,11 +1,66 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func main() {
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
+var commands map[string]cliCommand
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func init() {
+	commands = getCommands()
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) > 0 {
+			if command, ok := commands[words[0]]; ok {
+				err := command.callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
 
 func cleanInput(text string) []string {
@@ -13,4 +68,21 @@ func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	words := strings.Fields(text)
 	return words
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for cmdName, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmdName, cmd.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
